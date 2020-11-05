47727fe0f4727071be24aa4b0f294ea1d961e6bc
Mateus300 <mateus.souza.pro@gmail.com>
2020-11-05 12:44:45 -0300

    Adicionado atividade form - Sandra

---
+++ Atividade - 3 formularios (com eventos nos 3 formulários no primeiro input)/JS/efeitos.js
@@ -0,0 +1,8 @@
+function cor() {
+    var input = document.getElementById("nome")
+    
+    input.style.color = "blue"; //Muda a cor do input que esta digitando
+
+    // input = document.body.style.backgroundColor = "blue"; //Muda a cor do body inteiro, tirando as div´s e fieldsets
+
+}
